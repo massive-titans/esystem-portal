c3bd69a9935110b1e2dd1486a66861c7fb45b9f7
frenzy007 <87853917+massive-titans@users.noreply.github.com>
2023-02-11 00:25:42 +0100

    Update globalObjects.js

--- server/helper/globalObjects.js
+++ server/helper/globalObjects.js
@@ -5,9 +5,21 @@ const accountType = {
 };
 
 const indexNavigation = {
-  myDashboard: { content: "My Dashboard", link: "#" },
-  myCourses: { content: "My Courses", link: "/api/courses/my_courses" },
-  siteAdmin: { content: "Site Administration", link: "/admin/site_admin" },
+  myDashboard: {
+    id: "dashboard",
+    content: "My Dashboard",
+    link: "#",
+  },
+  myCourses: {
+    id: "courses",
+    content: "My Courses",
+    link: "/api/courses/my_courses",
+  },
+  siteAdmin: {
+    id: "site_admin",
+    content: "Site Administration",
+    link: "/admin/site_admin",
+  },
 };
 
 module.exports = { accountType, indexNavigation };
