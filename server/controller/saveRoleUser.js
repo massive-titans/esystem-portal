1b91be2fa6d4f1470180f0ff1366ec302c4d0cdb
frenzy007 <87853917+massive-titans@users.noreply.github.com>
2023-03-01 23:05:32 +0100

    Update saveRoleUser.js

--- server/controller/saveRoleUser.js
+++ server/controller/saveRoleUser.js
@@ -1,4 +1,5 @@
 const _ = require("lodash");
+const connection = require("../database/connection");
 
 const globalObjects = require("../helper/globalObjects");
 const Account = require("../model/account");
@@ -7,55 +8,63 @@ const Teacher = require("../model/teacher");
 const Admin = require("../model/admin");
 const DynamicProfile = require("../model/dynamicRef");
 
-async function saveAccount(obj) {
-  const account = new Account(
-    _.pick(obj, ["email", "password", "accountType"])
-  );
-  return await account.save();
-}
-
 module.exports = async function (role, obj) {
-  if (role == globalObjects.accountType.student) {
-    const resultAccount = await saveAccount(obj);
-    const infoDetails = _.pick(obj, ["firstName", "lastName", "birthdate"]);
-    infoDetails.accountLogin = _.pick(resultAccount, ["_id"]);
+  // start transaction session
+  const session = await connection.startSession();
+  try {
+    // start transaction
+    session.startTransaction();
+    const account = new Account(
+      _.pick(obj, ["email", "password", "accountType"])
+    );
+    await account.save({session});
 
-    const student = new Student(infoDetails);
+    // check which role is the assign to new user, then navigate to correct role table
+    if (role == globalObjects.accountType.student) {
+      const infoDetails = _.pick(obj, ["firstName", "lastName", "birthdate"]);
+      infoDetails.accountLogin = _.pick(account, ["_id"]);
 
-    const resultSavedStudent = await student.save();
-    const dynamicProfile = new DynamicProfile({
-      userId: resultSavedStudent._id,
-      accountType: "Student",
-    });
-    await dynamicProfile.save();
+      const student = new Student(infoDetails);
+      await student.save({session});
+
+      const dynamicProfile = new DynamicProfile({
+        userId: student._id,
+        accountType: "Student",
+      });
+      await dynamicProfile.save({session});
+    }
+    if (role == globalObjects.accountType.teacher) {
+      const infoDetails = _.pick(obj, ["firstName", "lastName", "birthdate"]);
+      infoDetails.accountLogin = _.pick(account, ["_id"]);
+
+      const teacher = new Teacher(infoDetails);
+      await teacher.save({session});
+
+      const dynamicProfile = new DynamicProfile({
+        userId: teacher._id,
+        accountType: "Teacher",
+      });
+      await dynamicProfile.save({session});
+    }
+    if (role == globalObjects.accountType.admin) {
+      const infoDetails = _.pick(obj, ["firstName", "lastName", "birthdate"]);
+      infoDetails.accountLogin = _.pick(account, ["_id"]);
+
+      const admin = new Admin(infoDetails);
+      await admin.save({session});
+
+      const dynamicProfile = new DynamicProfile({
+        userId: admin._id,
+        accountType: "Admin",
+      });
+      await dynamicProfile.save({session});
+    }
+
+    //Finish Transaction
+    await session.commitTransaction();
+  } catch (error) {
+    await session.abortTransaction();
+    console.log("Failed to create new user!");
   }
-  if (role == globalObjects.accountType.teacher) {
-    const resultAccount = await saveAccount(obj);
-    const infoDetails = _.pick(obj, ["firstName", "lastName", "birthdate"]);
-    infoDetails.accountLogin = _.pick(resultAccount, ["_id"]);
-
-    const teacher = new Teacher(infoDetails);
-
-    const resultSavedTeacher = await teacher.save();
-    const dynamicProfile = new DynamicProfile({
-      userId: resultSavedTeacher._id,
-      accountType: "Teacher",
-    });
-    await dynamicProfile.save();
-  }
-
-  if (role == globalObjects.accountType.admin) {
-    const resultAccount = await saveAccount(obj);
-    const infoDetails = _.pick(obj, ["firstName", "lastName", "birthdate"]);
-    infoDetails.accountLogin = _.pick(resultAccount, ["_id"]);
-
-    const admin = new Admin(infoDetails);
-
-    const resultSavedAdmin = await admin.save();
-    const dynamicProfile = new DynamicProfile({
-      userId: resultSavedAdmin._id,
-      accountType: "Admin",
-    });
-    await dynamicProfile.save();
-  }
+  session.endSession();
 };
